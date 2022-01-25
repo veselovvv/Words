70c5e14e827d71ef10d99aa3ea7d639442f215ce
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2022-01-25 20:04:56 +0200

    Cleaned up code

--- app/src/main/java/com/veselovvv/words/WordsViewHolder.kt
+++ app/src/main/java/com/veselovvv/words/WordsViewHolder.kt
@@ -4,12 +4,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.veselovvv.words.databinding.ListItemBinding
 import com.veselovvv.words.db.Word
 
-class WordsViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-
+class WordsViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(word: Word, clickListener: (Word) -> Unit) {
         binding.wordTextView.text = word.word
         binding.translateTextView.text = word.translate
-
         binding.listItemLayout.setOnClickListener { clickListener(word) }
     }
 }
