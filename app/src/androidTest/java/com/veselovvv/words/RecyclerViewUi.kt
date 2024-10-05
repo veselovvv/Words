505dd0c4c74575543af4caddebe06b04bc586c5e
veselovvv <vycheslav5veselov@gmail.com>
2024-10-05 19:10:07 +0300

    Added ui objects

---
+++ app/src/androidTest/java/com/veselovvv/words/RecyclerViewUi.kt
@@ -0,0 +1,34 @@
+package com.veselovvv.words
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.veselovvv.words.RecyclerViewItemCountAssertion.withItemCount
+import org.hamcrest.Matchers.allOf
+
+class RecyclerViewUi {
+    private val interaction: ViewInteraction = onView(
+        allOf(
+            withId(R.id.recyclerView),
+            isAssignableFrom(RecyclerView::class.java)
+        )
+    )
+
+    fun checkInitialState() {
+        interaction.check(matches(withItemCount(0)))
+    }
+
+    fun checkWordsListState(words: List<Pair<String, String>>) {
+        words.forEachIndexed { index, (word, translation) ->
+            interaction.perform(scrollToPosition<RecyclerView.ViewHolder>(index))
+                .check(matches(isDisplayed()))
+                .check(matches(withRecyclerViewItemText(R.id.word_text_view, word)))
+                .check(matches(withRecyclerViewItemText(R.id.translate_text_view, translation)))
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/veselovvv/words/TextFieldUi.kt
@@ -0,0 +1,23 @@
+package com.veselovvv.words
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+
+class TextFieldUi {
+    fun checkIsVisible(id: Int) {
+        onView(withId(id)).check(matches(isDisplayed()))
+    }
+
+    fun typeIn(id: Int, text: String) {
+        onView(withId(id)).perform(clearText()).perform(typeText(text))
+    }
+
+    fun checkState(id: Int, text: String) {
+        onView(withId(id)).check(matches(withText(text)))
+    }
+}
